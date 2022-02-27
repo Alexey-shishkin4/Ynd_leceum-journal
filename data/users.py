260f155f455ff81761dfb9896ace13a9681b7c90
Djazzy-06 <alexisv.shishkin@gmail.com>
2022-02-27 12:59:14 +0700

    master

--- data/users.py
+++ data/users.py
@@ -1,6 +1,7 @@
 import datetime
 import sqlalchemy
 from sqlalchemy import orm
+from werkzeug.security import generate_password_hash, check_password_hash
 from .db_session import SqlAlchemyBase
 
 
@@ -20,4 +21,11 @@ class User(SqlAlchemyBase):
     modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
 
     news = orm.relation("Jobs", back_populates='user')
+
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
     
